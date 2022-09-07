23d3a5606c1e1e29a69ac662cf533700c9bc8acb
hgrafa <hugorafell@gmail.com>
2022-09-07 18:19:19 -0300

    Adding Services Layer

---
+++ projects/Car Rental Service/Car Rental Service/Services/BrasilTaxService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Rental_Service.Services {
+    public class BrasilTaxService : ITaxService {
+
+        public double Tax(double amount) {
+
+            if (amount > 100.0) {
+                return 0.15 * amount;
+            }
+
+            return 0.20 * amount;
+        }
+    }
+}
---
+++ projects/Car Rental Service/Car Rental Service/Services/RentalService.cs
@@ -0,0 +1,35 @@
+﻿using Car_Rental_Service.Domain;
+
+namespace Car_Rental_Service.Services {
+    public class RentalService {
+
+        public double PricePerHour { get; set; }
+        public double PricePerDay { get; set; }
+
+        private ITaxService _taxService;
+
+        public RentalService(double pricePerHour, double pricePerDay, ITaxService taxService) {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+            _taxService = taxService;
+        }
+
+        public void ProcessInvoice(CarRental carRental) {
+
+            TimeSpan duration = carRental.End.Subtract(carRental.Start);
+
+            double basicPayment = 0.0, taxPayment = 0.0;
+
+            if (duration.TotalHours <= 12.0) {
+                basicPayment = Math.Ceiling(duration.TotalHours) * PricePerHour;
+            } else {
+                basicPayment = Math.Ceiling(duration.TotalDays) * PricePerDay;
+            }
+
+            taxPayment = _taxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, taxPayment);
+        }
+
+    }
+}
