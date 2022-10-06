6e57429dcca90b1b7255e66d99103151828a2d3d
hgrafa <hugorafell@gmail.com>
2022-10-06 15:55:41 -0300

    Adding data structure project

---
+++ projects/EstruturasDeDados/EstruturasDeDados/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturasDeDados {
+    public class Pessoa {
+
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public Pessoa(string name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public override bool Equals(object? obj) {
+            return obj is Pessoa pessoa &&
+                   name == pessoa.name &&
+                   age == pessoa.age;
+        }
+
+        public override int GetHashCode() {
+            return HashCode.Combine(name, age);
+        }
+    }
+}
---
+++ projects/EstruturasDeDados/EstruturasDeDados/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace EstruturasDeDados {
+
+    public class Program {
+
+        public static void Main() {
+
+            HashSet<Pessoa> list = new HashSet<Pessoa>();
+
+            Pessoa p1 = new Pessoa("joao", 20);
+            Pessoa p2 = new Pessoa("joao", 20);
+
+            list.Add(p1);
+            list.Add(p2);
+
+            Pessoa p;
+            if (!list.TryGetValue(p1, out p)) {
+                Console.WriteLine("value doesn't exist");
+            } else {
+                Console.WriteLine(p);
+            }
+
+            list.RemoveWhere(p => p.age >= 20);
+
+            Console.WriteLine(list.Count) ;
+
+
+
+            Dictionary<string, int> idades = new Dictionary<string, int>();
+
+            idades.Add(p1.name, p1.age);
+
+            idades["joao"] = 21;
+
+            foreach( KeyValuePair<string, int> pessoa in idades ) {
+                Console.WriteLine($"{pessoa.Key} = {pessoa.Value}");
+            }
+
+            Console.WriteLine(idades["joao"]);
+
+        }
+    }
+
+}
